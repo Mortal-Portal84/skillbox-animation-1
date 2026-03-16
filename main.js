f33605d03d0413ad39ece755e002536cfd585d7c
Andrei Yeutukhou <andrei.yeutukhou@gmail.com>
2026-03-16 17:31:06 +0300

    add burger button animation

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const header = document.querySelector('.header')
-const burgerBtn = document.querySelector('.header__burger')
+const burgerBtn = document.querySelector('.burger')
 const headerMenu = document.querySelector('.header__menu')
 
 burgerBtn.addEventListener('click', () => {
   header.classList.toggle('header--open')
   headerMenu.classList.toggle('header__menu--opened')
   document.body.classList.toggle('scroll-lock')
+  burgerBtn.classList.toggle('burger--opened')
 })
